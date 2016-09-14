ec7c9dba7441e96a89cee4bf3d63aabca922473e
Vinoth Kannan <vinothkannan@vinkas.com>
2016-09-14 20:36:25 +0530

    new: vinkas authentication server logics

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Traits\AuthenticatesUsers;
+
+class AuthenticationController extends Controller
+{
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+}
